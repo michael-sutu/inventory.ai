876db6bddfeb0e573d7dfea25c882d0323aeb445
MikeJMS8910 <61876275+MikeJMS8910@users.noreply.github.com>
2023-06-21 16:16:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,181 +1,270 @@
-const fetch = require('node-fetch-commonjs')
 const express = require("express")
-const { initializeApp } = require('firebase/app')
-const bodyParser = require('body-parser')
-const { getFirestore, setDoc, doc, getDoc, query, getDocs, collection } = require('firebase/firestore/lite')
+const multer = require('multer')
+const path = require("path")
+const fs = require("fs")
+const { MongoClient, ServerApiVersion } = require('mongodb')
+const md5 = require('md5')
+const fetch = require("node-fetch-commonjs")
 const app = express()
-app.use(bodyParser.urlencoded({ extended: false }))
 
-const accountSid = "AC1eea0a6f6943c336f1cb94961b25dc51"
-const authToken = "377f78e0572fcaac67be57f4a069855f"
-const number = "whatsapp:+15165189647"
-const twilio = require('twilio')(accountSid, authToken)
-const { MessagingResponse } = require('twilio').twiml
+app.listen(1000)
+console.log("Listening at http://localhost:1000")
 
-app.listen(5000)
+let storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, __dirname + '/Resources/User-Images/')
+  },
+  filename: function (req, file, cb) {
+    const extension = path.extname(file.originalname)
+    cb(null, Date.now() + extension)
+  }
+})
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDG2ffZQhmO0xFmFPTy4Gt_CS8tSQD7Aw0",
-    authDomain: "shepherd-a1e51.firebaseapp.com",
-    projectId: "shepherd-a1e51",
-    storageBucket: "shepherd-a1e51.appspot.com",
-    messagingSenderId: "497639325389",
-    appId: "1:497639325389:web:fe2848ffa4c09af9237384",
-    measurementId: "G-855TE47MY1"
-}
-initializeApp(firebaseConfig)
-const db = getFirestore()
+let upload = multer({ storage: storage })
+let type = upload.single('upl')
 
-const read = function () {
-    return new Promise(function (resolve, reject) {
-        let final = {}
-        const collectionRef = collection(db, 'whatsapp')
-        getDocs(collectionRef)
-            .then((querySnapshot) => {
-                querySnapshot.forEach((doc) => {
-                    final[doc.id] = doc.data()
-                })
-                resolve(final)
-            })
-            .catch((error) => {
-                console.log(error)
-                reject(error)
-            })
-    })
-}
-  
-const write = function (docId, newData) {
-    return new Promise(function (resolve, reject) {
-        const collectionRef = collection(db, 'whatsapp')
-        const docRef = doc(collectionRef, docId)
-        setDoc(docRef, newData)
-            .then(() => {
-                resolve('Data written successfully.')
-            })
-            .catch((error) => {
-                console.error(error)
-                reject(error)
-            })
-        })
+const url = "mongodb+srv://michaelsutu:AJV95ixy12LSkBfA@inventoryai.1lwuiqu.mongodb.net/?retryWrites=true&w=majority"
+const client = new MongoClient(url)
+
+function sendF(file, res) {
+	return res.sendFile(__dirname+`/${file}`)
 }
 
-let getResponse = function (messageHistory, newMessage) {
-    return new Promise(function (resolve, reject) {
-        let gptMessages = [{ "role": "user", "content": "You are a friendly tool to help people follow their path in Christianity. Here are some rules and examples that you should follow: You should always provide advice based on the essence of Bible verses. While you may add secular advice that aligns with the teachings of the verse, it should never give opinionated secular advice. You empathize briefly before providing the verse. You can personalize your messages by using the word you. For sensitive topics, you should focus on providing Bible verses and explaining their meanings, without offering extensive secular advice. In responses to queries, you should focus on a primary verse that is most relevant to the query or topic and a summary of what the verse means. In dealing with sensitive or controversial topics, you should exercise caution. You should offer uplifting verses, refrain from subjective advice, and recommend that the user talk to a trusted individual about their issues. If a query is unrelated to the Bible, you should remind users that your there to assist with Bible-related questions and cannot provide expertise in unrelated areas. You should avoid using first-person language to maintain your role as a tool or resource, not a conversational partner. For example, instead of saying 'I suggest reading this verse...', the you could say 'Consider this verse...' or This verse might be helpful...'. The language should still be friendly and accessible, but you should not create an impression of having your own thoughts or feelings. Do you understand?" }, { "role": "assistant", "content": "Yes, I understand and will follow all of these rules and examples that you have set for me." }]
-        if(messageHistory.length <= 9) {
-            for(let i = 0; i < messageHistory.length; i++) {
-                gptMessages.push(messageHistory[i])
-            }
+function makeid(length) {
+  let result = ''
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  const charactersLength = characters.length
+  let counter = 0
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength))
+    counter += 1
+  }
+  return result
+}
+
+app.get("/", (req, res) => {
+	sendF("Public/index.html", res)
+})
+
+let validFiles = ["script.js", "styles.css", "module.js", "bubbles.css"]
+app.get("/file/:path", (req, res) => {
+	if(validFiles.includes(req.params.path)) {
+		sendF("Resources/"+req.params.path, res)
+	} else {
+		res.json({"Code": 404, "Message": "Page not found."})
+	}
+})
+
+let validImages = ["logo.png", "favicon.png", "title.png", "google.png", "facebook.png", "placeholder.png", "placeholder2.png", "xsold.png", "sold.png", "xsettings.png", "settings.png", "xaccounts.png", "accounts.png", "xbox.png", "box.png", "balance.png", "xbalance.png", "amazon.jpg", "bonanza.png", "ebay.png"]
+app.get("/image/:path", (req, res) => {
+	if(validImages.includes(req.params.path)) {
+		sendF("Resources/Images/"+req.params.path, res)
+	} else {
+		res.json({"Code": 404, "Message": "Page not found."})
+	}
+})
+
+app.get("/user-images/:path", (req, res) => {
+	sendF("Resources/User-Images/"+req.params.path, res)
+})
+
+app.get("/api/account", async (req, res) => {
+	let action = req.query.type
+	if(action == "new") {
+		await client.connect()
+		const db = client.db("users")
+		let current = await db.collection("people").findOne({"Email": req.query.email})
+		if(current == null) {
+			let pass = true
+			let private = makeid(15)
+			try {
+				await db.collection("people").insertOne({
+  				"Email": req.query.email,
+					"Password": req.query.password,
+					"Id": makeid(7),
+					"Private": private,
+					"Owns": []
+				})
+			} catch(error) {
+				pass = false
+				console.log(error)
+				res.json({"Code": 400, "Error": error})
+			}
+			if(pass == true) {
+				res.json({"Code": 200, "Message": "Successfully created new account.", "Private": private})
+			}
+		} else {
+			res.json({"Code": 401, "Error": "Email is already in use."})
+		}
+	} else if(action == "login") {
+		try {
+			const db = client.db("users")
+			let final = await db.collection("people").findOne({"Email": req.query.email})
+      if(final == null) {
+       	 res.json({"Code": 401.1, "Error": "Unknown email."})
+      } else {
+        if(final.Password == req.query.password) {
+        	res.json({"Code": 200, "Message": "Login Successful.", "Private": final.Private})
         } else {
-            for(let i = 0; i < 6; i++) {
-                gptMessages.push(messageHistory[i])
+          res.json({"Code": 401.2, "Error": "Invalid password."})
+        }
+      }
+		} catch(error) {
+			console.log(error)
+			res.json({"Code": 400, "Error": error})
+		}
+	} else if(action == "get") {
+		try {
+			await client.connect()
+			const db = client.db("users")
+			let pulled = await db.collection("people").findOne({"Private": req.query.private})
+			if(pulled == null) {
+				res.json({"Code": 401, "Error": "Unknown private."})
+			} else {
+				res.json({"Code": 200, "Data": {"Email": pulled.Email, "Owns": pulled.Owns}, "Message": "Successfully pulled user data."})
+			}
+		} catch(error) {
+			console.log(error)
+			res.json({"Code": 400, "Error": error})
+		}
+	}
+})
+
+app.post("/api/inventory", type, async (req, res) => {
+	let action = req.query.type
+	if(action == "new") {
+		try {
+			let updatedOwns = []
+			let newItem = {
+				"Image": req.file.filename,
+				"Name": req.query.name,
+				"Value": req.query.value
+			}
+			await client.connect()
+			const db = client.db("users")
+			let current = await db.collection("people").findOne({"Private": req.query.private})
+			if(current) {
+				current.Owns.push(newItem)
+				updatedOwns = current.Owns
+				const result = await db.collection("people").updateOne({ Private: req.query.private }, { $set: { Owns: updatedOwns }})
+				res.json({"Code": 200, "Message": "Successfully added item to inventory."})
+			} else {
+				res.json({"Code": 401, "Error": "Unkown private key."})
+			}
+		} catch(error) {
+			console.log(error)
+			res.json({"Code": 400, "Error": error})
+		}
+	}
+})
+
+app.get("/api/save-item", async (req, res) => {
+    try {
+        let updatedOwns = []
+		let newItem = {
+			"Image": req.query.image,
+			"Name": req.query.name,
+			"Min": req.query.min,
+            "Max": req.query.max,
+            "Quantity": req.query.quantity,
+            "Condition": req.query.condition
+		}
+		await client.connect()
+		const db = client.db("users")
+		let current = await db.collection("people").findOne({"Private": req.query.private})
+		if(current) {
+			current.Owns.push(newItem)
+			updatedOwns = current.Owns
+			await db.collection("people").updateOne({ Private: req.query.private }, { $set: { Owns: updatedOwns }})
+			res.json({"Code": 200, "Message": "Successfully added item to inventory."})
+		} else {
+			res.json({"Code": 401, "Error": "Unkown private key."})
+		}
+    } catch(error) {
+        console.log(error)
+        res.json({"Code": 400, "Error": error})
+    }
+})
+
+app.post("/api/get-value", type, (req, res) => {
+    let name = req.query.name
+    let condition = req.query.condition
+    let image = req.file.filename
+
+    let url = 'https://svcs.ebay.com/services/search/FindingService/v1'
+    let appID = 'MichaelS-inventor-PRD-f95f6f890-8fec23b2'
+    let request = {
+        'OPERATION-NAME': 'findCompletedItems',
+        'SERVICE-VERSION': '1.13.0',
+        'SECURITY-APPNAME': appID,
+        'RESPONSE-DATA-FORMAT': 'JSON',
+        'keywords': name,
+        'itemFilter(0).name': 'SoldItemsOnly',
+        'itemFilter(0).value': 'true'
+    }
+
+    let queryString = Object.keys(request)
+        .map(function(key) {
+            return key + '=' + encodeURIComponent(request[key])
+        })
+        .join('&')
+
+    fetch(url + '?' + queryString)
+        .then(function(response) {
+            if (response.ok) {
+                return response.json()
+            } else {
+                console.log(response)
+                throw new Error(response.status)
+            }
+        })
+        .then(function(data) {
+            let results = []
+            let items = data.findCompletedItemsResponse[0].searchResult[0].item
+
+            for(let i = 0; i < items.length; i++) {
+				results.push({"Name": items[i].title, "Price": items[i].sellingStatus[0].currentPrice[0].__value__, "Image": items[i].galleryURL})
             }
 
-            for(let i = messageHistory.length - 3; i < messageHistory.length; i++) {
-                gptMessages.push(messageHistory[i])
-            }
-        }
-        gptMessages.push(newMessage)
-    
-        let requestOptions = {
-             method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + "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"
-            },
-            body: JSON.stringify({
-                model: 'gpt-3.5-turbo',
-                messages: gptMessages,
-                max_tokens: 750,
-                temperature: 0.1
-            })
-        }
-    
-        fetch("https://api.openai.com/v1/chat/completions", requestOptions)
-            .then(response => response.json())
-            .then(data => {
-                console.log(gptMessages)
-                console.log(data)
-                resolve(data.choices[0].message.content)
-            })	
-            .catch(error => {
-                console.log(error)
-                reject(error)
-            })
-    })
-}
+			fetch(`https://feature-matching.onrender.com?condition=${condition}&start=https://inventory-ai.onrender.com/user-images/${image}`, {
+					method: "POST",
+					body: JSON.stringify(results)
+				})
+					.then(response => response.json())
+					.then(data => {
+						res.json({"min": data.min, "max": data.max, "image": req.file.filename})
+					})
+					.catch(error => console.log(error))
+        })
+        .catch(function(error) {
+            console.log(error.message)
+        })
+})
 
-let sendMessage = function (to, body) {
-    return new Promise(async function (resolve, reject) {
-        try {
-            await twilio.messages.create({ from: number, body: body, to: to})
-        } catch(error) {
-            console.log(error)
-        }
-        resolve("Success")
-    })
-}
-
-let handleInbound = function (body, from) {
-    return new Promise(async function (resolve, reject) {
-        try {
-            if(body !== "") {
-                let current = await read()
-                let user = current[from.replace("whatsapp:+", "")]
-                let newResponse = ""
-                let newStep = 0
-                if(user !== undefined) {
-                    if(user.Step == 0) {
-                        newResponse = "What's your name?"
-                        await sendMessage(from, newResponse)
-                        newStep = 1
-                    } else if(user.Step == 1) {
-                        newResponse = `Thanks, ${body}. And do you have a preference for the bible version? e.g. NIV / KJV. (It's also fine if you don't)`
-                        await sendMessage(from, newResponse)
-                        newStep = 2
-                    } else if(user.Step == 2) {
-                        newResponse = `Great - you're all set up. Is there anything on your mind today?`
-                        await sendMessage(from, newResponse)
-                        newStep = 3
-                    } else {
-                        let response = await getResponse(user.History, {"role": "user", "content": body})
-                        newResponse = response
-                        await sendMessage(from, newResponse)
-                        newStep = 3
-                    }
-                    user.History.push({"role": "user", "content": body})
-                    user.History.push({"role": "assistant", "content": newResponse})
-                    let updatedUser = {
-                        "Number": user.Number,
-                        "Step": newStep,
-                        "History": user.History
-                    }
-                    write(from.replace("whatsapp:+", ""), updatedUser)
-                    resolve("Success")
-                } else {
-                    let newNumber = from.replace("whatsapp:+", "")
-
-                    try {
-                        let current = await read()
-                        if(current[newNumber] == undefined) {
-                            await sendMessage(`whatsapp:+${newNumber}`, "Welcome to Shepherd! It's a pleasure to have you on board. You can message at any time with questions or queries. All responses are all based on the Word of the Bible. Let's grow together 🌱.")
-                            await sendMessage(`whatsapp:+${newNumber}`, "What's your name?")
-                            await write(newNumber.replace("whatsapp:+", ""), {"History": [{"role": "assistant", "content": "Welcome to Shepherd! It's a pleasure to have you on board. You can message at any time with questions or queries. All responses are all based on the Word of the Bible. Let's grow together 🌱."}, {"role": "assistant", "content": "What's your name?"}], "Number": newNumber, "Step": 1})
-                        } else {
-                            await sendMessage(`whatsapp:+${newNumber}`, "Looks like you tried to initiate a chat with me again. You can just directly talk to me here.")
-                        }
-                    } catch (error) {
-                        console.error(error)
-                    }
+app.get("/api/delete-item", async (req, res) => {
+    try {
+        let identifier = req.query.image.split("/")[req.query.image.split("/").length - 1]
+		await client.connect()
+		const db = client.db("users")
+		let current = await db.collection("people").findOne({"Private": req.query.private})
+		if(current) {
+            for(let i = 0; i < current.Owns.length; i++) {
+                if(current.Owns[i].Image == identifier) {
+                    current.Owns.splice(i, 1)
+                    break
                 }
             }
-        } catch(error) {
-            console.log("Error: "+error)
-        }
-    })
-}
+			await db.collection("people").updateOne({ Private: req.query.private }, { $set: { Owns: current.Owns }})
+			res.json({"Code": 200, "Message": "Successfully deleted item from inventory."})
+		} else {
+			res.json({"Code": 401, "Error": "Unkown private key."})
+		}
+    } catch(error) {
+        console.log(error)
+        res.json({"Code": 400, "Error": error})
+    }
+})
 
-app.post("/get-message", (req, res) => {
-    handleInbound(req.body.Body, req.body.From)
-    res.send(req.body.Body)
+app.get("*", (req, res) => {
+	res.json({"Code": 404, "Message": "Page not found."})
 })
