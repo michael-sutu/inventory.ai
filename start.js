de302a3b8ef42dfd496f30d844c86c2ec251690d
MikeJMS8910 <61876275+MikeJMS8910@users.noreply.github.com>
2023-06-23 21:03:48 -0400

    Update start.js

--- start.js
+++ start.js
@@ -1,13 +1,6 @@
 const concurrently = require('concurrently')
-
-concurrently([
+const { result } = concurrently([
     { command: 'pip install -r install.txt', name: 'Flask Build', prefixColor: 'cyan' },
     { command: 'node index.js', name: 'Node.js Server', prefixColor: 'cyan' },
     { command: 'gunicorn main:app', name: 'Flask Server', prefixColor: 'cyan' }
 ])
-    .then(() => {
-        console.log('Both servers are online.')
-    })
-    .catch((err) => {
-        console.error('Error starting servers:', err)
-    })
