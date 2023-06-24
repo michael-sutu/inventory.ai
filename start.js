c0cf31da816eb563078f223801bcb384c50ea69e
MikeJMS8910 <61876275+MikeJMS8910@users.noreply.github.com>
2023-06-23 21:29:28 -0400

    Update start.js

--- start.js
+++ start.js
@@ -2,21 +2,13 @@ const concurrently = require('concurrently')
 
 async function runCommands() {
     try {
-      await concurrently([
-        { command: 'pip install -r install.txt', name: 'Flask Build', prefixColor: 'cyan' }
-      ])
+        await concurrently([
+            { command: 'node index.js', name: 'Node.js Server', prefixColor: 'cyan' }
+        ])
   
-      await concurrently([
-        { command: 'node index.js', name: 'Node.js Server', prefixColor: 'cyan' }
-      ])
-  
-      setTimeout(async (e) => {
         await concurrently([
             { command: 'gunicorn main:app', name: 'Flask Server', prefixColor: 'cyan' }
         ])
-      }, 120000)
-      
-  
       console.log('All commands completed successfully.')
     } catch (error) {
       console.error('One or more commands failed:', error)
